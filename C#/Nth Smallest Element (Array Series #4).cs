cc73cd98b28cffb9828a1d43c11d6ed94261117c
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-03 15:58:28 +0100

    Add files via upload

---
+++ C#/Nth Smallest Element (Array Series #4).cs
@@ -0,0 +1,10 @@
+﻿namespace Nth_Smallest_Element__Array_Series__4_
+{
+    internal class Kata
+    {
+        public static int NthSmallest(int[] arr, int pos)
+        {
+            return arr.OrderBy(n => n).ElementAt(pos - 1);
+        }
+    }
+}
