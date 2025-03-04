8ec0eee1eb3bd8659f277d2aeb2e49d8232c4672
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-04 12:36:08 +0100

    Add files via upload

---
+++ C#/Product Of Maximums Of Array (Array Series #2).cs
@@ -0,0 +1,19 @@
+﻿
+internal class Kata
+{
+    public static int MaxProduct(int[] arr, int size)
+    {
+        //var product = 1;
+
+        //Array.Sort(arr);
+
+        //for (var i = arr.Length - 1; i >= arr.Length - size; i--)
+        //{
+        //    product *= arr[i];
+        //}
+
+        //return product;
+
+        return arr.OrderBy(i => -i).Take(size).Aggregate((x, y) => x * y);
+    }
+}
