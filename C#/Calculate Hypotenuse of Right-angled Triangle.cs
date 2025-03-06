d1188916b05e4950b901a4fe91cbea736acaaab6
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-06 16:56:23 +0100

    Add files via upload

---
+++ C#/Calculate Hypotenuse of Right-angled Triangle.cs
@@ -0,0 +1,15 @@
+﻿
+internal class Kata
+{
+    public static double CalculateHypotenuse(double a, double b)
+    {
+        //a ^ 2 + b ^ 2 = c ^ 2
+
+        if (a <= 0 || b <= 0 || double.IsNaN(a) || double.IsNaN(b))
+            throw new ArgumentException();
+
+        double d = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        double result = Math.Round(d, 3);
+        return result;
+    }
+}
