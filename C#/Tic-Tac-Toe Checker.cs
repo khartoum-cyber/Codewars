9d1dad23dba48784ab18c3431e77f6e0b0368321
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-12 12:56:27 +0100

    Add files via upload

---
+++ C#/Tic-Tac-Toe Checker.cs
@@ -0,0 +1,30 @@
+﻿
+internal class Kata
+{
+    public int IsSolved(int[,] board)
+    {
+        bool zeroFound = false;
+        List<int[]> lines = new List<int[]>();
+
+        lines.Add(new int[3] { board[0, 0], board[1, 0], board[2, 0] });
+        lines.Add(new int[3] { board[0, 1], board[1, 1], board[2, 1] });
+        lines.Add(new int[3] { board[0, 2], board[1, 2], board[2, 2] });
+        lines.Add(new int[3] { board[0, 0], board[0, 1], board[0, 2] });
+        lines.Add(new int[3] { board[1, 0], board[1, 1], board[1, 2] });
+        lines.Add(new int[3] { board[2, 0], board[2, 1], board[2, 2] });
+        lines.Add(new int[3] { board[0, 0], board[1, 1], board[2, 2] });
+        lines.Add(new int[3] { board[2, 0], board[1, 1], board[0, 2] });
+
+        foreach (var line in lines)
+        {
+            if (line.All(n => n == 1))
+                return 1;
+            if (line.All(n => n == 2))
+                return 2;
+            if (line.Contains(0))
+                zeroFound = true;
+        }
+
+        return zeroFound ? -1 : 0;
+    }
+}
