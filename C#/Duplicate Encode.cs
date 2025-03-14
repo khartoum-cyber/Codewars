c5dfbff32e62c0a5ca3141b5c9dfc93a9901eee5
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-14 09:41:16 +0100

    Add files via upload

---
+++ C#/Duplicate Encode.cs
@@ -0,0 +1,29 @@
+﻿
+internal class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        word = word.ToLower();
+
+        var newString = "";
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        foreach (var ch in word)
+        {
+            if (dict.ContainsKey(ch))
+                dict[ch]++;
+            else
+                dict[ch] = 1;
+        }
+
+        foreach (var ch in word)
+        {
+            if (dict[ch] == 1)
+                newString += "(";
+            else
+                newString += ")";
+        }
+
+        return newString;
+    }
+}
