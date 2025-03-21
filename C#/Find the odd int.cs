3dea1236c6a12a4ba7df5a5c97ce51100f70d620
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-21 14:28:19 +0100

    Add files via upload

---
+++ C#/Find the odd int.cs
@@ -0,0 +1,11 @@
+﻿
+internal class Kata
+{
+    public static int find_it(int[] seq)
+    {
+        return seq.GroupBy(n => n)
+            .Where(g => g.Count() % 2 != 0)
+            .Select(g => g.Key)
+            .FirstOrDefault();
+    }
+}
