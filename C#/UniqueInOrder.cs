f1e1593ff6e4a53d8f2251a6cebc566703bcf7bf
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-25 08:46:44 +0100

    Add files via upload

---
+++ C#/UniqueInOrder.cs
@@ -0,0 +1,20 @@
+﻿
+internal class Kata
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        T target = default(T);
+        List<T> result = new List<T>();
+
+        foreach (var element in iterable)
+        {
+            if (EqualityComparer<T>.Default.Equals(target, default(T)) || !EqualityComparer<T>.Default.Equals(target, element))
+            {
+                target = element;
+                result.Add(element);
+            }
+        }
+
+        return result;
+    }
+}
