354cee59f9309eb3f96ac12f595c925d578cc1d7
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-25 09:11:41 +0100

    Add files via upload

---
+++ C#/HighestAndLowest.cs
@@ -0,0 +1,9 @@
+﻿
+internal class Kata
+{
+    public static string HighAndLow(string numbers)
+    {
+        var numberList = numbers.Split(' ').Select(int.Parse).ToList();
+        return $"{numberList.Max()} {numberList.Min()}";
+    }
+}
