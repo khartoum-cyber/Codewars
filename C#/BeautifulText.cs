a9446f6fa2f796218b9a78d6f550852f6645e831
bartek0 <77171865+khartoum-cyber@users.noreply.github.com>
2025-03-28 09:09:34 +0100

    Add files via upload

---
+++ C#/BeautifulText.cs
@@ -0,0 +1,39 @@
+﻿
+internal class Kata
+{
+    public bool BeautifulText(string s, int l, int r)
+    {
+        string[] arr = s.Split();
+
+        for (int i = l; i <= r; i++)
+        {
+            Queue<string> q = new Queue<string>(arr);
+            List<string> lines = new List<string>();
+
+            while (q.Count > 0)
+            {
+                string line = "";
+
+                while (line.Length < i && q.Count > 0)
+                {
+                    string word = q.Dequeue();
+
+                    if (!string.IsNullOrEmpty(line))
+                        line += " ";
+
+                    line += word;
+                }
+
+                lines.Add(line);
+
+                if (line.Length != i)
+                    break;
+            }
+
+            if (lines.All(line => line.Length == i))
+                return true;
+        }
+
+        return false;
+    }
+}
